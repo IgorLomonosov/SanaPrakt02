f7b057d03f1603c2ea6b25e2cc7f647f20b37253
Ігор Ломоносов <ipz224_lio@student.ztu.edu.ua>
2022-11-12 12:17:12 +0200

    Змінено текст перших трьох рядків

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("Never gonna give you up");
+Console.WriteLine("Never gonna let you down");
+Console.WriteLine("Never gonna run around and desert you");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
