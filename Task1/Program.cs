ed87753aa3eb835b9d0bcb3198d94f72edd25c35
Ігор Ломоносов <ipz224_lio@student.ztu.edu.ua>
2022-11-12 12:18:52 +0200

    Змінено текст останніх двох рядків

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("Never gonna let you down");
 Console.WriteLine("Never gonna run around and desert you");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Never gonna make you cry");
+Console.WriteLine("Never gonna say goodbye");
